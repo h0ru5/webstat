bd61e603c27bf465dd4f766a6ed7ae368fe48d87
h0ru5 <johannes.hund@gmail.com>
2014-03-31 22:14:53 +0200

    added crude ws impl into testcontroller

--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,39 @@
 angular.module('remoteDev', ['ngRoute'])
     .config(function ($routeProvider, $locationProvider) {
+        'use strict';
         $routeProvider
             .when('/netstat', {
-                templateUrl: 'partials/netstat.html'
-                //,controller: 'NetstatController'
+                templateUrl: 'partials/netstat.html', 
+                controller: 'TestCtrl'
             })
             .otherwise({
                 templateUrl: 'partials/home.html'
             });
             
         $locationProvider.html5Mode(true);
-    });
+    })
+    .controller('TestCtrl', ['$scope', '$window', function ($scope, $window) {
+        'use strict';
+        var sock = null; 
+        
+        var incoming = function (event) {
+            $scope.$apply(function () {
+                $scope.log += event.data + '\n';
+            });
+        };
+        
+        $scope.log = '';
+        $scope.host = 'johanneshund.de';
+        $scope.port = 8088;
+        
+        $scope.connect = function (host, port) {
+            $scope.log += 'connecting to ws://' + host + ':' + port + '\n';
+            sock = new WebSocket('ws://' + host + ':' + port);
+            sock.onmessage = incoming;
+            sock.onopen = function () { $scope.log += 'opened\n'; };
+        };
+        
+        $scope.disconnect = function () {
+            sock.close();
+        };
+    }]);
