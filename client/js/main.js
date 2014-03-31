4bf71accb0b7a82915a6a26916ea1a349976541e
h0ru5 <johannes.hund@gmail.com>
2014-03-31 21:09:25 +0200

    added ng-routing

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,13 @@
-
+angular.module('remoteDev', ['ngRoute'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/netstat', {
+                templateUrl: 'partials/netstat.html'
+                //,controller: 'NetstatController'
+            })
+            .otherwise({
+                templateUrl: 'partials/home.html'
+            });
+            
+        $locationProvider.html5Mode(true);
+    });
